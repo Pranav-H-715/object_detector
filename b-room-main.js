7d8a31b1d5999c7143bd7418834924a6a849e182
Pranav-H-715 <75574260+Pranav-H-715@users.noreply.github.com>
2022-05-24 12:58:09 +0530

    Add files via upload

---
+++ b-room-main.js
@@ -0,0 +1,50 @@
+img = "";
+Status = "";
+objects = [];
+function preload() {
+    img = loadImage('b-room-img.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    document.getElementById("status").innerHTML = "Status : Object is Detecting";
+    ObjectDetector = ml5.objectDetector("cocossd", modelLoaded);
+}
+function draw() {
+    image(img, 0, 0, 600, 400);
+    if (Status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        ObjectDetector.detect(img, gotResults);
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object is Detected";
+            document.getElementById("number").innerHTML = "Number of Objects Detected are : " + objects.length;
+            percentage = Math.floor(objects[i].confidence * 100) + "%";
+            label = objects[i].label;
+            fill(r, g, b);
+            text(label + " " + percentage, objects[i].x + 20, objects[i].y + 20)
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].height, objects[i].width);
+        }
+    }
+}
+function back() {
+    window.location = "index.html";
+}
+function modelLoaded() {
+    console.log("cocoSsd initialized");
+    Status = true;
+    ObjectDetector.detect(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
---
+++ kitchen-main.js
@@ -0,0 +1,46 @@
+img = "";
+Status = "";
+
+function preload() {
+    img = loadImage('kitchen-img.jpeg');
+}
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    document.getElementById("status").innerHTML = "Status : Object is Detecting";
+    ObjectDetector = ml5.objectDetector("cocossd", modelLoaded);
+}
+function draw() {
+    image(img, 0, 0, 600, 400);
+    if (Status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(img, gotResult);
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Number of Objects Detected are : " + objects.length;
+            percentage = Math.floor(objects[i].confidence * 100) + "%";
+            label = objects[i].label;
+            fill(r,g,b);
+            text(label + " " + percentage, objects[i].x + 20, objects[i].y + 20)
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].height, objects[i].width);
+        }
+    }
+}
+function back(){
+    window.location = "index.html";
+}
+function modelLoaded() {
+    console.log("cocoSsd initialized");
+    Status = true;
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+        console.log(results);
+        objects = results;
+    }
